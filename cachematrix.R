d8661d5c5efdfe4e94b4e9c666a60cd774955bb9
coderoarke <roarke.mitchell@gmail.com>
2015-02-21 14:34:10 -0800

    Completed and commented.

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+##    For the sake of simplicity and time, this script is similar to the assignment example.  This script creates two functions:
 
-## Write a short comment describing this function
+##    1) "makeCacheMatrix" allows the instantiation of a special matrix object that carries a matrix and its inverse.  The special  
+##    matrix object can be read and set, and so can its inverse (stored in the "inv" variable).
 
 makeCacheMatrix <- function(x = matrix()) {
+      
             inv <- NULL
             set <- function(y) {
                   x <<- y
                   inv <<- NULL
             }
             get <- function() x
-            setinverse <- function(solve) inv <<- inv
+            setinverse <- function(inv) inv <<- inv
             getinverse <- function() inv
             list(set = set, get = get,
                  setinverse = setinverse,
                  getinverse = getinverse)
+            
 }
 
-
-## Write a short comment describing this function
+##    2) "cacheSolve" takes a special matrix object defined in "makeCacheMatrix" and solves it using R's "solve()" function, returning 
+##    its inverse.  If the special matrix object has already had its inverse calculated, the function will retrieve that object's inverse
+##    value, print an indication to the console that it is doing so, and skip the unnecessary calculation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
       
       inv <- x$getinverse()
       if(!is.null(inv)) {
